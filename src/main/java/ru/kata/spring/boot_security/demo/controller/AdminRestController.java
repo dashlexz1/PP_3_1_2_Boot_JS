e0ae7d2227faee64f351881a9da86b5ca1738e9d
dashlexz1 <adr1999@icloud.com>
2024-05-23 03:37:25 +0300

    CommitJS1

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -1,7 +1,6 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.UserDTO.UserDto;
@@ -25,25 +24,6 @@ public class AdminRestController {
         this.userMapper = userMapper;
     }
 
-    //    @GetMapping()
-//    public String showUsers(ModelMap model, Principal principal) {
-//        User currentUser = userService.findByUsername(principal.getName());
-//        model.addAttribute("users", userService.getAllUsers());
-//        model.addAttribute("currentUser", currentUser);
-//        model.addAttribute("allRoles", roleService.findAll());
-//        StringBuilder sb1 = new StringBuilder();
-//        for (Role r : userService.findById(currentUser.getId()).getUserRoles()) {
-//            if (r.getRoleName().equals("ROLE_ADMIN")) {
-//                sb1.append("ADMIN ");
-//            }
-//            if (r.getRoleName().equals("ROLE_USER")) {
-//                sb1.append("USER ");
-//            }
-//        }
-//        model.addAttribute("authUserRoles", sb1.toString());
-//
-//        return "adminspage";
-//    }
     @GetMapping
     public ResponseEntity<List<UserDto>> read() {
         List<UserDto> users = userService.getAllUsers().stream()
@@ -100,51 +80,3 @@ public class AdminRestController {
 }
 
 
-//    @GetMapping("/new")
-//    public ModelAndView newUser() {
-//        User user = new User();
-//        ModelAndView mav = new ModelAndView("adminspage");
-//        mav.addObject("user", user);
-//        Set<Role> roles = roleService.findAll();
-//        mav.addObject("allRoles", roles);
-//        return mav;
-//    }
-
-
-//    @PostMapping("/new")
-//    public String addUser(@ModelAttribute("user") UserDto userDto) {
-//        User user = userMapper.toModel(userDto);
-//        userService.add(user);
-//        return "redirect:/admin";
-//    }
-//
-//    @GetMapping("/removeUser")
-//    public String removeUser(@RequestParam(value = "id") Long id) {
-//        logger.info("GET запрос на удаление пользователя: {}", id);
-//        userService.delete(id);
-//        logger.info("Пользователь {} успешно удален", id);
-//        return "redirect:/admin";
-//    }
-//}
-
-//    @GetMapping("/{id}/edit")
-//    public ModelAndView editUser(@PathVariable(name = "id") Long id) {
-////        User user = userService.findById(id);
-//        ModelAndView mav = new ModelAndView("adminspage");
-////        mav.addObject("user", user);
-//        return mav;
-//    }
-
-//    @PostMapping("/{id}")
-//    public String postEditUserForm(@PathVariable Long id, @ModelAttribute("user") UserDto userdto) {
-//        User user = userMapper.toModel(userdto);
-//        if(user.getPassword() == null) {
-//            user.setPassword(userService.findById(id).getPassword());
-//        }
-//        logger.info("POST запрос на редактирование пользователя: {}", user);
-//        userService.update(user);
-//        logger.info("Пользователь с id: {} успешно обновлен", user.getId());
-//        return "redirect:/admin";
-//    }
-//
-//}
